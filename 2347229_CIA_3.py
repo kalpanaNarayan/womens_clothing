869598761c9af947d8ae80dc7bb48ef23c4f5b70
Kalpana <139973681+kalpanaNarayan@users.noreply.github.com>
2024-04-16 10:51:39 +0530

    Update 2347229_CIA_3.py

--- 2347229_CIA_3.py
+++ 2347229_CIA_3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
